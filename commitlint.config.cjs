c616135fff6134094aa413d549860fdd806dacbb
kyoufei <keyaofei@126.com>
2023-07-26 14:20:15 +0800

    # 请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交 # 说明将会终止提交。 # # 位于分支 master # 要提交的变更： #	修改：     .husky/commit-msg #	修改：     .husky/pre-commit #	修改：     commitlint.config.cjs #	修改：     package-lock.json #	修改：     package.json #

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,45 +1,43 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  extends: ['@commitlint/config-conventional', 'cz'],
+  extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
       2,
       'always',
       [
-        'feature', // 新功能（feature）
-        'bug', // 此项特别针对bug号，用于向测试反馈bug列表的bug修改情况
+        'feat', // 新功能（feature）
         'fix', // 修补bug
         'ui', // 更新 ui
         'docs', // 文档（documentation）
         'style', // 格式（不影响代码运行的变动）
-        'perf', // 性能优化
-        'release', // 发布
-        'deploy', // 部署
         'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
+        'perf', // 性能优化
         'test', // 增加测试
-        'chore', // 构建过程或辅助工具的变动
+        'deploy', // 部署
+        'ci', // 集成
         'revert', // feat(pencil): add ‘graphiteWidth’ option (撤销之前的commit)
+        'chore', // 构建过程或辅助工具的变动
         'merge', // 合并分支， 例如： merge（前端页面）： feature-xxxx修改线程地址
-        'build', // 打包
       ],
     ],
-    // <type> 格式 小写
-    'type-case': [2, 'always', 'lower-case'],
-    // <type> 不能为空
-    'type-empty': [2, 'never'],
-    // <scope> 范围不能为空
-    'scope-empty': [2, 'never'],
-    // <scope> 范围格式
-    'scope-case': [0],
-    // <subject> 主要 message 不能为空
-    'subject-empty': [2, 'never'],
-    // <subject> 以什么为结束标志，禁用
-    'subject-full-stop': [0, 'never'],
-    // <subject> 格式，禁用
-    'subject-case': [0, 'never'],
-    // <body> 以空行开头
-    'body-leading-blank': [1, 'always'],
-    'header-max-length': [0, 'always', 72],
+    // // <type> 格式 小写
+    // 'type-case': [2, 'always', 'lower-case'],
+    // // <type> 不能为空
+    // 'type-empty': [2, 'never'],
+    // // <scope> 范围不能为空
+    // 'scope-empty': [2, 'never'],
+    // // <scope> 范围格式
+    // 'scope-case': [0],
+    // // <subject> 主要 message 不能为空
+    // 'subject-empty': [2, 'never'],
+    // // <subject> 以什么为结束标志，禁用
+    // 'subject-full-stop': [0, 'never'],
+    // // <subject> 格式，禁用
+    // 'subject-case': [0, 'never'],
+    // // <body> 以空行开头
+    // 'body-leading-blank': [1, 'always'],
+    // 'header-max-length': [0, 'always', 72],
   },
   prompt: {
     messages: {
@@ -70,6 +68,7 @@ module.exports = {
       { value: "ci",       name: "集成:     🎡  修改 CI 配置、脚本",  emoji: ":ferris_wheel:"},
       { value: "revert",   name: "回退:     ⏪️  回滚 commit",emoji: ":rewind:"},
       { value: "chore",    name: "其他:     🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）", emoji: ":hammer:"},
+      { value: "merge",    name: "合并:     🕸️  合并分支", emoji: ":spider web:"},
     ],
     useEmoji: true,
     emojiAlign: 'center',
